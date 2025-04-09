0464c9472807dc3c06fc513d9c38af9cb000b1e0
Dstar18 <dedybintang18@gmail.com>
2025-04-10 08:38:37 +0900

    fix: validation store

--- controllers/article_controller.go
+++ controllers/article_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -16,6 +17,29 @@ type ArticleValidate struct {
 
 func ArticlesStore(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		var article ArticleValidate
+
+		// Check request body
+		if err := c.Bind(&article); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{
+				"code":    http.StatusBadRequest,
+				"message": "Invalid request body",
+			})
+		}
+
+		// Validation struct
+		validate := validator.New()
+		if err := validate.Struct(article); err != nil {
+			errors := make(map[string]string)
+			for _, err := range err.(validator.ValidationErrors) {
+				errors[err.Field()] = "This field is" + " " + err.Tag() + " " + err.Param()
+			}
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{
+				"code":    http.StatusBadRequest,
+				"message": errors,
+			})
+		}
+
 		// return success
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"code":    http.StatusOK,
