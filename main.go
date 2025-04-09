89966387d94e3561279461f377b9bb973b904787
Dstar18 <dedybintang18@gmail.com>
2025-04-09 17:08:18 +0900

    fix: container app and connect DB

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"GoArticle/config"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// connect DB
+	config.InitDB()
+
+	// initialize echo
 	e := echo.New()
 
 	e.GET("/hello", func(c echo.Context) error {
